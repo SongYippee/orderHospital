a06677e2783160a862bab2b6aa62f5846e0e2f97
Yippee Song <34080396+SongYippee@users.noreply.github.com>
2019-06-27 12:20:02 +0800

    update SuDaHospital class
    To support order specified date

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from mylogger import MyLogger
 
 MyLogger(show_console=True)
 
+
 class SuDaHospital(object):
     '''苏州大学第一附属医院，十梓街院'''
 
@@ -46,21 +47,26 @@ class SuDaHospital(object):
             "response_type": '9'
         }
         response = self.session.post(login_url, headers=self.headers, data=data, timeout=60)
-        print "login " + str(response.status_code) + " " + response.url
+        logging.info(u"登录状态 %s, URL：%s" % (str(response.status_code), response.url))
         self.session.cookies.update(response.cookies)
+        if response.status_code != 200:
+            logging.error(u"登录失败")
+            sys.exit(0)
         if response.content.find(self.user['name'].encode('utf-8')):
-            print "login success"
             status = True
         else:
-            print "login failed"
             status = False
         return status
 
     def __logout(self):
         req = self.session.get("http://new.sdfyy.cn/preregister/logout.html")
-        print "logout " + str(req.status_code) + " " + req.url
+        print logging.info(u"退出")
 
-    def __initDoctorURL(self):
+    def __initDoctor(self):
+        '''
+        初始化医生，关键是获取doctor的code和url
+        :return:
+        '''
         searchUrl = "http://new.sdfyy.cn/appointment/localhost/sdfyy/index.php?s=/Appointment/search"
         self.headers['Referer'] = 'http://new.sdfyy.cn/appointment/index.html'
         self.headers['X-Requested-With'] = 'XMLHttpRequest'
@@ -71,29 +77,41 @@ class SuDaHospital(object):
         }
         response = self.session.post(searchUrl, data=post_data, headers=self.headers)
         self.headers.pop('X-Requested-With')
-        print "init doc url " + str(response.status_code) + " " + response.url
+        logging.info(u"查询医生状态：%s, URL: %s" % (str(response.status_code), response.url))
+        if response.status_code != 200:
+            logging.error(u"查询医生失败" + response.text)
+            sys.exit(0)
 
-        def parseContent(content):
-            result = json.loads(content)
-            if result['code'] != 200:
-                print 'error'
-                sys.exit(0)
-            code = result['data'][0]['code']
-            return code
-
-        code = parseContent(response.content)
+        result = json.loads(response.content)
+        if result['code'] != 200:
+            logging.error(u'初始化医生信息失败\n')
+            sys.exit(0)
+        code = result['data'][0]['code']
+        deptcode = result['data'][0]['deptcode']
+        deptname = result['data'][0]['deptname']
         docURL = "http://new.sdfyy.cn/appointment/index.php?s=/doctor/index/id/" + str(code)
         self.doctor['code'] = code
         self.doctor['url'] = docURL
+        self.doctor['deptname'] = deptname
+        self.doctor['deptcode'] = deptcode
 
     def __orderDoctor(self):
         if 'url' not in self.doctor:
-            self.__initDoctorURL()
+            self.__initDoctor()
         self.headers['Referer'] = 'http://new.sdfyy.cn/appointment/index.html'
+        # 访问医生页面，获取可预约的日期
         response = self.session.get(self.doctor['url'], headers=self.headers)
-        print "retrieve doctor url " + str(response.status_code) + " " + response.url
+        logging.info(u"访问医生页面状态: %s, URL: %s" % (str(response.status_code), response.url))
+        if response.status_code != 200:
+            logging.error(u"访问医生页面失败" + response.text)
+            sys.exit(0)
 
-        def getDateList(content):
+        def getCanOrderDateList(content):
+            '''
+            查询医生可预约日期列表
+            :param content:
+            :return:
+            '''
             a = re.findall(r"\<span class=\"hospital-name ...\"\>(.*?)\<\/span\>", content)
             # print a
             b = re.findall(r"\<span class=\"typename\"\>(.*?)\<\/span\>", content)
@@ -131,7 +149,12 @@ class SuDaHospital(object):
                     dateList.append(detail)
             return dateList
 
-        def getTimeList(dateDetail):
+        def getCanOrderTimeList(dateDetail):
+            '''
+            查询指定日期内，可预约的时间段
+            :param dateDetail:
+            :return:
+            '''
             timeUrl = "http://new.sdfyy.cn/appointment/index.php?s=/Home/Interface/worktime&callback=timedetailcallback&unit_code=" + str(
                 dateDetail['unitCode']) + "&doctor_code=" + self.doctor['code'] + "&charge_type=" + dateDetail[
                           'chargeType'] + "+&ampm=" + dateDetail['ampm'] + "&request_day=" + dateDetail[
@@ -141,7 +164,7 @@ class SuDaHospital(object):
             self.headers['X-Requested-With'] = 'XMLHttpRequest'
             response = self.session.get(timeUrl, headers=self.headers)
             self.headers.pop('X-Requested-With')
-            print "retrieve doctor time list " + str(response.status_code) + " " + response.url
+            logging.info(u"查询 %s 可预约时段状态: %s, URL: %s" % (dateDetail['date'], str(response.status_code), response.url))
 
             def parseTimeDetail(content):
                 content = content[content.find('(') + 1:-1]
@@ -157,24 +180,30 @@ class SuDaHospital(object):
 
             return parseTimeDetail(response.content)
 
-        dateList = getDateList(response.content)
+        dateList = getCanOrderDateList(response.content)
+
+        if self.user['date']:
+            dateList = [x for x in dateList if x['date'] == self.user['date']]
+
         if dateList:
             for eachDate in dateList:
-                timeList = getTimeList(eachDate)
-                if not timeList:
-                    print 'no time can order on ' + eachDate['date'] + ' for doctor ' + self.doctor['name']
-                for eachTime in timeList:
-                    if self.__realOrder(eachDate, eachTime):
-                        print 'ordered on ' + eachDate['date'] + " " + eachTime['starttime'] + "-" + eachTime[
-                            'endtime'] + ' for doctor ' + self.doctor['name']
-                        return
+                timeList = getCanOrderTimeList(eachDate)
+                if timeList:
+                    for eachTime in timeList:
+                        if self.__realOrder(eachDate, eachTime):
+                            logging.info(u"预约成功，%s %s - %s %s" % (eachDate['date'], eachTime['starttime'], eachTime[
+                                'endtime'], self.doctor['name']))
+                            return
+                else:
+                    logging.info(u'%s这天，%s医生的号已排满，无法预约' % (eachDate['date'], self.doctor['name']))
+                    continue
 
         else:
-            print 'no date can order for doctor ' + self.doctor['name']
+            logging.info(u"当前没有 %s 医生的可预约日期" % (self.doctor['name'],))
 
     def __realOrder(self, dateDetail, timeDetail):
         '''预约医生'''
-        urlBase = 'http://new.sdfyy.cn/preregister/reservesubmit?'
+        confirmPage = 'http://new.sdfyy.cn/preregister/reservesubmit?'
         data = {
             'hospital_name': dateDetail['hospitalName'].encode('utf-8'),
             'hospital_id': dateDetail['hospitalId'],
@@ -192,20 +221,26 @@ class SuDaHospital(object):
         }
         param = urlencode(data)
         # print param
-        order_url = urlBase + param
+        confirmPage = confirmPage + param
         # print order_url
         headers = dict()
         headers["Referer"] = self.doctor['url']
         self.headers.update(headers)
-        response = self.session.get(order_url, headers=self.headers)
-        print "realOrder " + str(response.status_code) + " " + response.url
+        # 预约信息确认的页面
+        response = self.session.get(confirmPage, headers=self.headers)
+        logging.info(u"确认信息页面状态：%s， URL: %s" % (str(response.status_code), response.url))
+        if response.status_code != 200:
+            logging.error(u"确认预约信息失败\n" + response.text)
+            sys.exit(0)
         self.headers.update(Referer=response.url)
 
-        # print response.text
-        def submit(htmlContent):
-            baseUrl = "http://new.sdfyy.cn/preregister"
-            content = htmlContent
-            result = re.findall(r"\$\('#preregister-submit'\).click\(function\(\)\{(.*)\$\.ajax", content, re.S)
+        def parseParams(htmlContent):
+            '''
+            解析确认页面，提取参数
+            :param htmlContent:
+            :return:
+            '''
+            result = re.findall(r"\$\('#preregister-submit'\).click\(function\(\)\{(.*)\$\.ajax", htmlContent, re.S)
             params = {}
             params['callback'] = 'submitcallback'
             for i in result:
@@ -217,25 +252,47 @@ class SuDaHospital(object):
                         value = items[1].strip().replace('\'', '').replace('\"', '')
                         params[key] = value.encode('utf-8')
             params['_'] = format(time.time() * 1000, '.0f')
+            return params
+
+        # print response.text
+        def confirmSubmit(params):
+            '''
+            确认提交，把预约信息提交给服务器，预约的最后一步
+            :param params:
+            :return:
+            '''
+            baseUrl = "http://new.sdfyy.cn/preregister"
             ajaxUrl = params.pop('getAjaxDetail')[1:]
             targetURL = baseUrl + ajaxUrl
-            response = self.session.get(targetURL, params=params, headers=self.headers)
-            print "submit " + str(response.status_code) + " " + response.url
-            if response.status_code == 200:
+            # 提交预约信息
+            orderResponse = self.session.get(targetURL, params=params, headers=self.headers)
+            logging.info(u"提交预约信息的状态：%s, URL: %s" % (str(orderResponse.status_code), orderResponse.url))
+            if orderResponse.status_code == 200:
                 # response message
-                message = response.text
+                message = orderResponse.text
                 message = message[message.find('(') + 1:-1]
-                print message
+                # print message
                 responseJson = json.loads(message)
-                print responseJson['code'], responseJson['msg']
+                # print responseJson['code'], responseJson['msg']
                 if responseJson['code'] == 201:
+                    # 预约成功，发送短信
                     self.sendSMS(phoneNo=responseJson['tel']['mobileno'], message=responseJson['tel']['msg'])
+                    return True
+                elif responseJson['code'] == 500:
+                    logging.error(u"服务器端错误,请在浏览器端重试: %s" % (responseJson['msg'],))
+                    return False
+                else:
+                    logging.error(u"提交预约信息失败\n" + responseJson)
+                    return False
+            else:
+                logging.error(u"提交预约信息失败\n" + orderResponse.text)
+                return False
 
-        # submit(response.text)
-        return True
+        params = parseParams(response.text)
+        return confirmSubmit(params)
 
-    def sendSMS(self, phoneNo="your phone number", message="your message"):
-        '''发送短信，经过测试，不用登陆也能发短信'''
+    def sendSMS(self, phoneNo=u"手机号", message=u"短信内容"):
+        '''发送短信'''
         smsUrl = "http://new.sdfyy.cn/preregister/localhost/sdfyy/index.php?s=/Appointment/sendSms"
         headers = {"X-Requested-With": "XMLHttpRequest"}
         self.headers.update(headers)
@@ -244,15 +301,15 @@ class SuDaHospital(object):
             "mobileno": phoneNo,
             "msg": message
         }
-        response = self.session.post(smsUrl, data=data, headers=self.headers)
-        print "sendSMS " + str(response.status_code) + " " + response.url
-        print response.content
+        sendSMSResponse = self.session.post(smsUrl, data=data, headers=self.headers)
+        logging.info(u"发送短信的状态：%s, URL: %s" % (str(sendSMSResponse.status_code), sendSMSResponse.url))
+        logging.info(sendSMSResponse.content)
         self.headers.pop("X-Requested-With")
 
     def order(self):
-        self.__login()
-        self.__orderDoctor()
-        self.__logout()
+        if self.__login():
+            self.__orderDoctor()
+            self.__logout()
 
 
 class SZ12320(object):
@@ -486,6 +543,8 @@ class SZ12320(object):
             # print "step04 get checkvalue error"
             logging.error("step04 get checkvalue error")
             sys.exit(0)
+
+        # 最终的确定预约
         # response = self.session.post(url, data={'checkvalue': checkvalue, 't': format(time.time() * 1000, '.0f')},
         #                              headers=self.headers)
         # print "step04_post_action " + str(response.status_code) + " " + response.url
@@ -503,31 +562,32 @@ class SZ12320(object):
 
 
 if __name__ == "__main__":
-    print 'start'
-    sz12320user = {
-        'id': u'your ID number',
-        'name': u'your name',
-        'type': u'市民卡',  # 自费，市民卡
-        'phone': u'phone number'
-    }
-    sz12320doctor = {
-        'name': u"doctor name",
-        'hospital': u'苏州大学附属儿童医院',
-        'date': u'2019-02-28',  # todo 支持选日期和时间段
-        'time': u'09-10'
-    }
-    sz = SZ12320(user=sz12320user, doctor=sz12320doctor)
-    sz.order()
+    print u'开始执行。。。。。。'
+    # sz12320user = {
+    #     'id': u'身份证号',
+    #     'name': u'姓名',
+    #     'type': u'市民卡',  # 自费，市民卡
+    #     'phone': u'手机号'
+    # }
+    # sz12320doctor = {
+    #     'name': u"医生姓名",
+    #     'hospital': u'医院名字',
+    #     'date': u'date',  # todo 支持选日期和时间段
+    #     'time': u'09-10' # 08-09 代表 8 点至 9 点
+    # }
+    # sz = SZ12320(user=sz12320user, doctor=sz12320doctor)
+    # sz.order()
 
-    # sduser = {
-    #     'id': 'identity card number',
-    #     'name': 'your name',
-    #     'password': 'your password'
-    # }
-    #
-    # sddoctor = {
-    #     'name': u'成兴波'
-    # }
-    #
-    # sd = SuDaHospital(user=sduser, doctor=sddoctor)
-    # sd.order()
+    sduser = {
+        'id': '身份证号',
+        'name': u'姓名',
+        'password': '密码',
+        'date': '2019-07-11'
+    }
+
+    sddoctor = {
+        'name': u'医生姓名'
+    }
+
+    sd = SuDaHospital(user=sduser, doctor=sddoctor)
+    sd.order()
